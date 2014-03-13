3f5f48219fee7bc566ac532d90e7625d7442abf6
Eric Socolofsky <eric@transmote.com>
2014-03-12 22:25:32 -0700

    Initial Commit

---
+++ GratinTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GratinTomatoes
+//
+//  Created by Eric Socolofsky on 3/12/14.
+//  Copyright (c) 2014 Eric Socolofsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GratinTomatoesTests/GratinTomatoesTests.m
@@ -0,0 +1,34 @@
+//
+//  GratinTomatoesTests.m
+//  GratinTomatoesTests
+//
+//  Created by Eric Socolofsky on 3/12/14.
+//  Copyright (c) 2014 Eric Socolofsky. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GratinTomatoesTests : XCTestCase
+
+@end
+
+@implementation GratinTomatoesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
